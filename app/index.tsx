220cb76f139c3271049f893c0e8ca15987c3fea9
pr4gya-ai <your_email@example.com>
2026-06-12 16:48:14 +0530

    updated files

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Build a Full Stack Mobile App and Publish to App Store</Text>
+    </View>
+  );
+}
